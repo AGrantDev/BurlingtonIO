1e8c9c8601528a72bd9c76b9d84d15d6a9936aaf
Anthony Grant <grant@christians-mbp.attlocal.net>
2023-08-17 10:56:19 -0500

    update hero, style changes to global configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -259,6 +259,7 @@ module.exports = {
             DEFAULT: '1',
         },
         fontFamily: {
+            'sans': ['ui-sans-serif', 'system-ui'],
             display: ['"Albert Sans", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
             body: ['"Albert Sans", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
         },
